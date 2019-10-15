d19d5c9e0d49194fa6e1a3317426f33f4fd6891a
BIT <BIT@DESKTOP-LRHLNNS>
2019-10-15 14:54:33 +0900

    xml 및 java config

---
+++ src/main/java/config/videosystem/VideoSystemConfig02.java
@@ -0,0 +1,14 @@
+package config.videosystem;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+
+import kr.co.itcen.springcontainer.videosystem.DVDPlayer;
+
+@Configuration
+@Import({ DVDPlayerConfig2.class })
+@ImportResource({ "classpath:config/videosystem/DVDConfig.xml" })
+public class VideoSystemConfig02 {
+
+}
---
+++ src/main/java/kr/co/itcen/springcontainer/videosystem/DVDPack.java
@@ -0,0 +1,15 @@
+package kr.co.itcen.springcontainer.videosystem;
+
+import java.util.List;
+
+public class DVDPack {
+	private String title;
+	private String studio;
+	private List<DigitalVideoDisc> dvds;
+	
+	public DVDPack(String title, String studio, List<DigitalVideoDisc> dvds) {
+		this.title = title;
+		this.studio = studio;
+		this.dvds = dvds;
+	}
+}
